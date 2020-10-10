13637a0247500af85f07a269ead223170c9d6de1
lina mierkE <lina_maria89@hotmail.com>
2020-10-10 17:16:03 -0400

    API images

---
+++ script.js
@@ -0,0 +1,106 @@
+// let url = "https://www.googleapis.com/books/v1/volumes?q=horror+subject:mystery&key=AIzaSyBurcL0Vnq50D-vV1kMvd-LMuoPLf0XBag"
+
+
+let baseURL = "https://www.googleapis.com/books/v1/volumes?q="
+let genre = "horror"
+let subject = "mystery"
+let keyURL = "&key=AIzaSyBurcL0Vnq50D-vV1kMvd-LMuoPLf0XBag"
+let fullURL = baseURL + genre + "+subject:" + subject + keyURL
+let mystery = document.querySelector('.tab1')
+let romance = document.querySelector('.tab2')
+let fiction = document.querySelector('.tab3')
+let fantasy = document.querySelector('.tab4')
+
+
+
+fetch(fullURL)
+   .then(data => data.json())
+   .then ( data => {
+      
+      let image1 = data.items[0].volumeInfo.imageLinks.thumbnail
+      mystery.addEventListener("click", function (e) {
+         e.preventDefault();
+         var img = document.createElement('img');
+         img.src = image1;
+         img.width = 500;
+         img.height = 500;
+         mystery.appendChild(img)
+         let news = document.querySelector('.content')
+         news.appendChild(img)
+      
+   })
+})
+
+
+let genre1 = "love"
+let subject1 = "love"
+// let keyURL = "&key=AIzaSyBurcL0Vnq50D-vV1kMvd-LMuoPLf0XBag"
+let fullURL1 = baseURL + genre1 + "+subject:" + subject1 + keyURL
+
+fetch(fullURL1)
+   .then(data => data.json())
+   .then ( data => {
+      
+      let image1 = data.items[8].volumeInfo.imageLinks.thumbnail
+      romance.addEventListener("click", function (e) {
+         e.preventDefault();
+         var img2 = document.createElement('img');
+         img2.src = image1;
+         img2.width = 500;
+         img2.height = 500;
+         romance.appendChild(img2)
+         let news2 = document.querySelector('.content')
+         news2.appendChild(img2)
+      
+   })
+})
+
+let genre2 = "fiction"
+let subject2 = "fiction"
+// let keyURL = "&key=AIzaSyBurcL0Vnq50D-vV1kMvd-LMuoPLf0XBag"
+let fullURL2 = baseURL + genre2 + "+subject:" + subject2 + keyURL
+
+fetch(fullURL2)
+   .then(data => data.json())
+   .then ( data => {
+      
+      let image1 = data.items[1].volumeInfo.imageLinks.thumbnail
+      fiction.addEventListener("click", function (e) {
+         e.preventDefault();
+         var img3 = document.createElement('img');
+         img3.src = image1;
+         img3.width = 500;
+         img3.height = 500;
+         romance.appendChild(img3)
+         let news3 = document.querySelector('.content')
+         news3.appendChild(img3)
+      
+   })
+})
+
+let genre3 = "fantasy"
+let subject3 = "fantasy"
+// let keyURL = "&key=AIzaSyBurcL0Vnq50D-vV1kMvd-LMuoPLf0XBag"
+let fullURL3 = baseURL + genre3 + "+subject:" + subject3 + keyURL
+
+fetch(fullURL3)
+   .then(data => data.json())
+   .then ( data => {
+      
+      let image2 = data.items[3].volumeInfo.imageLinks.thumbnail
+      fantasy.addEventListener("click", function (e) {
+         e.preventDefault();
+         var img4 = document.createElement('img');
+         img4.src = image2;
+         img4.width = 500;
+         img4.height = 500;
+         fantasy.appendChild(img4)
+         let news4 = document.querySelector('.content')
+         news4.appendChild(img4)
+      
+   })
+})
+
+
+
+
